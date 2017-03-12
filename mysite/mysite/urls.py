dc65aa6397eb9df936e07730aaf53e31e22342d1
poncheg <pon.otec@gmail.com>
2017-03-12 20:07:33 +0300

    Add project files.

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for mysite.
+"""
+
+from django.conf.urls import include, url
+from blog import *
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', mysite.views.home, name='home'),
+    url(r'^blog/', include(blog.views)),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+]
