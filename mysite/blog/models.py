a8fb3a184302bb85b65b361c0df63a2a8ae7738e
poncheg <pon.otec@gmail.com>
2017-03-24 22:01:29 +0300

    Template Django Step

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
-    test = models.TextField()
+    text = models.TextField()
     created_date = models.DateTimeField(
         default = timezone.now)
-    publish_date = models.DateTimeField(
+    published_date = models.DateTimeField(
         blank = True, null = True)
 
 def publish(self):
